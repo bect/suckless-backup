67c969767536154ddbdd511e3e39aea0e6bdf3cb
bect <bect1@live.com>
2026-05-15 23:48:40 +0700

    dwm: integrate premium Nordic Zen (Nord) color palette

--- dwm/config.h
+++ dwm/config.h
@@ -15,15 +15,16 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_nord0[]       = "#2e3440"; /* Deep Polar Night (Background) */
+static const char col_nord1[]       = "#3b4252"; /* Muted Slate (Inactive Border) */
+static const char col_nord4[]       = "#d8dee9"; /* Soft Snow (Normal Text) */
+static const char col_nord8[]       = "#88c0d0"; /* Frost Ice-Blue (Active Background) */
+static const char col_nord9[]       = "#81a1c1"; /* Glacial Blue (Active Window Border) */
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_nord4, col_nord0, col_nord1 },
+	[SchemeSel]  = { col_nord0, col_nord8, col_nord9 },
 };
 
 static const char *const autostart[] = {
@@ -75,7 +76,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_nord0, "-nf", col_nord4, "-sb", col_nord8, "-sf", col_nord0, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brupcmd[]  = { "brightnessctl", "set", "5%+", NULL };
 static const char *brdowncmd[] = { "brightnessctl", "set", "5%-", NULL };
