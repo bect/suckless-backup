a884097765e26aa66896819b1d75ca8517455e8f
bect <bect1@live.com>
2026-05-15 23:22:00 +0700

    dwm: add blueman-applet to native autostart roster for tray toggling

--- dwm/config.h
+++ dwm/config.h
@@ -26,6 +26,14 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+	"slstatus", NULL,
+	"nm-applet", NULL,
+	"blueman-applet", NULL,
+	"dunst", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
