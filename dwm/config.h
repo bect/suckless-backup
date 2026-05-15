dced70a158a7c5468ea842e6ed7d5b9aec9bc643
bect <bect1@live.com>
2026-05-16 00:33:26 +0700

    build: integrate integrated dm-power hub command and wire Alt+Escape trigger in dwm

--- dwm/config.h
+++ dwm/config.h
@@ -76,6 +76,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_cocoa0, "-nf", col_cocoa4, "-sb", col_merlot8, "-sf", col_cocoa0, NULL };
+static const char *powercmd[] = { "dm-power", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *brupcmd[]  = { "brightnessctl", "set", "5%+", NULL };
 static const char *brdowncmd[] = { "brightnessctl", "set", "5%-", NULL };
@@ -99,6 +100,7 @@ static const Key keys[] = {
 	{ 0,                            XF86XK_MonBrightnessUp,   spawn,          {.v = brupcmd } },
 	{ 0,                            XF86XK_MonBrightnessDown, spawn,          {.v = brdowncmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_Escape, spawn,          {.v = powercmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
